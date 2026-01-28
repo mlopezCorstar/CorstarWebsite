f676144a4ea656482e4a8de1cfac293bec237423
mlopezCorstar <mlopez@corstar.com>
2026-01-28 13:02:38 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/CorstarWebsite">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>
