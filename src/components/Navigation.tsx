8738311a7c610242b049ae327ae68c620505da60
mlopezCorstar <mlopez@corstar.com>
2026-01-28 12:02:52 -0500

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Menu, X, Phone } from 'lucide-react';
 
-const LOGO_PATH = '/image.png';
+const LOGO_PATH = '/CorstarWebsite/image.png';
 
 export default function Navigation() {
   const [isOpen, setIsOpen] = useState(false);
